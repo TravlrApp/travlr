37316f1bfd5164b382f5142712aefeed99cad352
Connor Taylor <conbond93@gmail.com>
2013-10-26 11:35:16 -0400

    general

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
       if @user.save
         session[:user_id] = @user.id
 
-        format.html { redirect_to posts_path, notice: 'User was successfully created.' }
+        format.html { redirect_to posts_path }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }

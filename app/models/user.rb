a0dfd8d67335b5b6d0e48fae13fd7b4f9a457076
Connor Taylor <conbond93@gmail.com>
2013-10-26 06:16:48 -0400

    fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	validates :last_name, presence: true
 	validates :age, presence: true
 	validates :email, presence: true, uniqueness: true
-	validates :password, presence: true, length: {minimum: 4, maximum: 20}, only:
+	validates :password, presence: true, length: {minimum: 4, maximum: 20}
 
 	has_many :posts
 	has_many :messages
